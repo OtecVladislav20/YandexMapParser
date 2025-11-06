178457bb9f19f000793d3ce74a948b7f4aebb038
Vladislav Maslakov <maslakov20@mail.ru>
2025-11-06 23:47:49 +0500

    init

---
+++ main.py
@@ -0,0 +1,8 @@
+from src.parser import YandexMapsParser
+
+def main():
+    parser = YandexMapsParser()
+    print("Парсер Яндекс карт запущен")
+
+if __name__ == "__main__":
+    main()
---
+++ src/parser.py
@@ -0,0 +1,7 @@
+class YandexMapsParser:
+    def __init__(self):
+        pass
+    
+    def parse_location(self, query):
+        # Ваша логика парсинга
+        pass
