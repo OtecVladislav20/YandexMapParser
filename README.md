d09d7faffac57ab7b8ff9ef1dfb5ec1098ac97e5
Vladislav Maslakov <maslakov20@mail.ru>
2025-11-07 17:31:58 +0500

    imitating a real client

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # YandexMapParser
+
+# 1. Создайте виртуальное окружение
+python -m venv venv
+
+# 2. Активация виртуального окружения
+.\venv\Scripts\Activate.ps1
+
+# 3. Установите зависимости
+pip install -r requirements.txt
--- src/scraper.py
+++ src/scraper.py
@@ -5,24 +5,18 @@ import time
 
 options = Options()
 options.add_argument('--headless=new')
-driver = webdriver.Chrome(
-    options=options, 
-    # другие свойства...
-) 
+options.add_argument("--user-data-dir=C:\\Temp\\ChromeProfile")
+options.add_argument("--no-sandbox")
 
-options.add_argument("--disable-blink-features=AutomationControlled")
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-options.add_experimental_option('useAutomationExtension', False)
-options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
 driver = webdriver.Chrome(options=options)
-driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
 
+try:
+    driver.get('https://yandex.ru/maps/org/blits_sam_/117320791407')
+    time.sleep(3)
 
-driver.get('https://yandex.ru/maps/org/blits_sam_/117320791407')
-time.sleep(3)
+    count = driver.find_element(By.CLASS_NAME, "business-header-rating-view__text")
+    text = count.text
+    print(text)
 
-count = driver.find_element(By.CLASS_NAME, "business-header-rating-view__text")
-text = count.text
-print(text)
-
-driver.quit()
+finally:
+    driver.quit()
